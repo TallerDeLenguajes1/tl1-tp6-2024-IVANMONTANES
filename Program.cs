7af07bb87489399c8d012fe48a1a9dd8432f6c69
ivan montañes <ivanmontanesfilipit@gmail.com>
2024-06-14 15:32:51 -0300

    cadenas concatenadas

--- Program.cs
+++ Program.cs
@@ -1,5 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
+//ejercicio longitud de cadena//
 Console.WriteLine("ingrese una cadena");
 string cadena = Console.ReadLine();
 int longitud = cadena.Length;
 Console.WriteLine("longitud "+longitud);
+//ejercicio concatenar cadena//
+Console.WriteLine("ingrese la segunda cadena:");
+string cadena2 = Console.ReadLine();
+string cadenaConcatenada = cadena+cadena2;
+Console.WriteLine("cadena concatenada: "+cadenaConcatenada);
+
+
