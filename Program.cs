e7bf707004ff1e9cbfb241b1b54ef08af0b99426
ivan montañes <ivanmontanesfilipit@gmail.com>
2024-06-14 14:26:40 -0300

    longitud cadena

--- Program.cs
+++ Program.cs
@@ -1,19 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-string cadenaNumero;
-int numero;
-Console.WriteLine("ingrese un numero:");
-cadenaNumero = Console.ReadLine();
-if(int.TryParse(cadenaNumero, out numero)){
-    int ultimoDigito;
-    int cociente = numero;
-    int invertido = 0;
-    while(cociente != 0){
-        ultimoDigito = cociente % 10;
-        cociente = cociente / 10;
-        invertido = invertido * 10 + ultimoDigito;
-    }
-    Console.WriteLine("numero invertido "+invertido);
-}
-else{
-    Console.WriteLine("no es un numero");
-}
+Console.WriteLine("ingrese una cadena");
+string cadena = Console.ReadLine();
+int longitud = cadena.Length;
+Console.WriteLine("longitud "+longitud);
