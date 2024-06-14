653de0264dc114484de75642f441d0e47f2c5db1
ivan montañes <ivanmontanesfilipit@gmail.com>
2024-06-14 15:38:21 -0300

    subcadena

--- Program.cs
+++ Program.cs
@@ -9,5 +9,13 @@ Console.WriteLine("ingrese la segunda cadena:");
 string cadena2 = Console.ReadLine();
 string cadenaConcatenada = cadena+cadena2;
 Console.WriteLine("cadena concatenada: "+cadenaConcatenada);
+//extraer subcadena//
+Console.WriteLine("indique el indice donde empezara la subcadena");
+string indiceCadena = Console.ReadLine();
+int indice;
+if(int.TryParse(indiceCadena,out indice)){
+    string subcadena = cadenaConcatenada.Substring(indice);
+    Console.WriteLine("subcadena "+subcadena);
+}
 
 
